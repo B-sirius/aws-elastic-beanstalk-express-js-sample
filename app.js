80548ebfc69bf53c3dbdaee6375c2e169f60a369
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2022-12-22 14:55:29 +0800

    feat: approval wording

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('====Lets learn AWS!!!===='));
+app.get('/', (req, res) => res.send('====Lets learn AWS, but still need approvals!!!===='));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
